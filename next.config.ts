4cd99a47e431afbf45deefd23dcda35e6e8250fe
Denis Chernykh <forgamesandforum@yandex.ru>
2025-04-16 18:15:57 +0500

    change config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	images: {
+		domains: ['hqajvfrvzozkuqhnxdyl.supabase.co'],
+	},
 	/* config options here */
 	experimental: {
 		serverActions: {
 			bodySizeLimit: '10mb',
 		},
+
 	},
 };
 
