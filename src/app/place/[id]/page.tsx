df389d05587da64803e173723cfce96ab537f1ea
Denis Chernykh <forgamesandforum@yandex.ru>
2025-04-16 17:23:46 +0500

    fix path in gallery

--- src/app/place/[id]/page.tsx
+++ src/app/place/[id]/page.tsx
@@ -1,12 +1,11 @@
-
-import Gallery from "@/components/features/gallery/Gallery";
+import Gallery from "../../../components/features/gallery/Gallery";
 import { getImagesById } from "@/repositories";
 
 import "swiper/css";
 
 async function PlacePage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-	const images = (await getImagesById(id)) || [];
+  const images = (await getImagesById(id)) || [];
   if (!images) return null;
   return (
     <div className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
