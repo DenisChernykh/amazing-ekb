fe3fdc368e9e9f86e2e2d055fc68c94a1bf2f7e8
Denis Chernykh <forgamesandforum@yandex.ru>
2025-05-13 13:51:22 +0500

    fix image url

--- src/utils/get-supabase-storage.ts
+++ src/utils/get-supabase-storage.ts
@@ -1,7 +1,8 @@
+const supabaseUrl = 'https://hqajvfrvzozkuqhnxdyl.supabase.co'
 export const getImageUrl = (path: string): string => {
 	if (process.env.NODE_ENV === 'development') {
 		return `${path}`
 	}
 
-	return `/storage/v1/object/public/post-images/${path}`
+	return `${supabaseUrl}/storage/v1/object/public/post-images/${path}`
 }
